eb1d4f9353fa7ccce54127ab7ec4700cd54561f1
Van Quoc Nguyen <quoc.nguyen@vanquoc.com>
2023-11-17 16:11:43 +0100

    Add test project.

---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet-Tests/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet_Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnsView()
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AppModelv2-WebApp-OpenIDConnect-DotNet-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AppModelv2-WebApp-OpenIDConnect-DotNet-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3d6dab50-4992-4d16-b8f7-d4c9d5af4128")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
